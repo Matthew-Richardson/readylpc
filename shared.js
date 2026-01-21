6a8b7f5b6379449cb6edd0595bd6480f2c5313c0
Matthew-Richardson <mdr.ls8@gmail.com>
2026-01-21 07:26:17 -0700

    Update shared.js

--- shared.js
+++ shared.js
@@ -190,7 +190,7 @@
                 <div class="announcement-accordion-callerid-value">${esc(data.callerId.voiceNumber)}</div>
               </div>` : ''}
             </div>
-            <button type="button" class="announcement-accordion-download-btn" data-action="download-vcard">>
+            <button type="button" class="announcement-accordion-download-btn" data-action="download-vcard">
               <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
               Download Contact Card
             </button>
