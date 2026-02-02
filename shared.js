754ac2821b64d916e3388a6b92d18600bf02140b
Matthew-Richardson <mdr.ls8@gmail.com>
2026-02-02 08:36:45 -0700

    Add files via upload

--- shared.js
+++ shared.js
@@ -324,7 +324,7 @@
   const VCARD_DATA = {
     name: 'CodeRED Alerts',
     org: 'La Plata County Emergency Management',
-    voiceNumber: '+12065363695',
+    voiceNumber: '+19703878700',
     textNumber: '38671'
   };
 
@@ -336,7 +336,7 @@
       `ORG:${VCARD_DATA.org}`,
       `TEL;TYPE=VOICE:${VCARD_DATA.voiceNumber}`,
       `TEL;TYPE=MSG:${VCARD_DATA.textNumber}`,
-      `NOTE:CodeRED emergency alert system for La Plata County. Voice calls come from (206) 536-3695 and text messages come from 386-71.`,
+      `NOTE:CodeRED emergency alert system for La Plata County. Voice calls come from (970) 387-8700 and text messages come from 386-71.`,
       'END:VCARD'
     ].join('\r\n');
     return vcard;
